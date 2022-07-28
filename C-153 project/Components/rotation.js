f4fa3058fa1344003f42975176b000ad1046db82
shikshith <71767952+shikshith@users.noreply.github.com>
2022-07-28 22:32:07 +0530

    Add files via upload

---
+++ C-153 project/Components/rotation.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent("terrain-rotation-reader", {
+    schema: {
+      speedOfRotation: { type: "number", default: 0 }
+  
+    },
+    init: function () {
+      window.addEventListener("keydown", (e) => {
+        if (e.key === "ArrowRight") {
+          if (this.data.speedOfRotation < 0.1) {
+            this.data.speedOfRotation += 0.01;
+          }
+        }
+        if (e.key === "ArrowLeft") {
+          if (this.data.speedOfRotation > -0.1) {
+            this.data.speedOfRotation -= 0.01;
+          }
+        }
+      });
+    },
+})
